f32cdb9102a5bc97afae0348dfc97b6f883a00fc
yxchun0 <1401248808@qq.com>
2017-10-20 16:22:03 +0800

    首页

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 App({
-  todos: [
-    { text: 'Learning Javascript', completed: true },
-    { text: 'Learning ES2016', completed: true },
-    { text: 'Learning 支付宝小程序', completed: false },
-  ],
   userInfo: null,
   getUserInfo() {
     return new Promise((resolve, reject) => {
@@ -30,4 +25,5 @@ App({
       });
     });
   },
+
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,9 @@
 import { post } from '../../config.js';
-import { ossPath} from '../../config.js';
+import { ossPath } from '../../config.js';
 
 Page({
   data: {
-    api:ossPath, 
+    api: ossPath,
     background: ['blue', 'red', 'yellow'],
     indicatorDots: true,
     vertical: false,
@@ -20,36 +20,51 @@ Page({
     tabbar: {},
     sheight: 0,
     stype: 'down',
-    order:''
+    order: ''
   },
   onLoad() {
-    console.log(this.data.api);
-    // 页面加载
     var that=this;
+    // my.getAuthCode({
+    //   scopes: 'auth_user',
+    //   success: (res) => {
+    //     console.log(res);
+    //     my.alert({
+    //       content: res.authCode,
+    //     });
+    //   },
+    // });
+    my.getSystemInfo({
+      success: (res) => {
+        console.log(res);
+        that.setData({
+          windowHeight:res.windowHeight
+        })
+      }
+    })
+    this.getdata();
+  },
+  getdata: function (lat, lng) {
+    // 页面加载
+    var that = this;
     post("/wxApi/c/index", { lat: '31.811226', lng: '119.974062' }, function (ret) {
-      
-      //优惠券
-      that.setData({
-        order: ret.data.order,
-        store: ret.data.store,
-        imgs: that.data.imgs,
-      })
+
       if (ret.data.coupons.length > 0) {
         var sendInfo = {};
         sendInfo.couponCanGet = ret.data.couponCanGet;
         sendInfo.couponTotalPrice = ret.data.couponTotalPrice;
         sendInfo.coupons = ret.data.coupons;
-        my.setStorageSync('sendInfo', sendInfo);
+        my.setStorageSync({keyi:'sendInfo',data:sendInfo});
         that.setData({
           sendInfo: sendInfo,
           sendShow: true
         })
       }
       if (ret.data.couponCanGet && ret.data.couponCanGet != '') {
-        var old = my.getStorageSync('couponCanGet');
+        var old = my.getStorageSync({key:'couponCanGet'}).data;
         var now = ret.data.couponCanGet.split(',');
         if (old) {
           now.forEach(function (o, i) {
+
             if (old.indexOf(o) == -1 && !that.data.sendShow) {
               that.setData({
                 robShow: true
@@ -59,6 +74,7 @@ Page({
           })
 
         } else {
+          console.log(that.data.sendShow);
           if (!that.data.sendShow) {
             that.setData({
               robShow: true,
@@ -129,166 +145,209 @@ Page({
           })
         }
       }
+      //停止下拉刷新
+      my.stopPullDownRefresh()
     })
 
   },
-  theme () {
-      var that = this;
-      post("/wxApi/c/theme", {}, function (ret) {
-        if (ret.code == 0) {
-          var btnArry = [];
-          var alias=[];
-          var num = ret.data.btnDc + ret.data.btnPd + ret.data.btnWm + ret.data.btnYd;
-          //首页四个按钮
-          var aliasPd = ret.data.aliasPd;
-          var aliasYd = ret.data.aliasYd;
-          var aliasWm = ret.data.aliasWm;
-          var aliasDc = ret.data.aliasDc;
-          var aliasQr = ret.data.aliasQr;
-          if (aliasQr){
-            my.setStorageSync({key:'aliasQr',data:aliasQr} );
-          }else{
-            my.removeStorageSync('aliasQr');
-          }
-          if (num == 3 && !ret.data.btnPd) {
-            if (ret.data.btnYd == 1) {
-              if (aliasYd != 1 && aliasYd){
-                that.setData({ aliasYd: aliasYd.substring(0, 4)});
-                btnArry.push(aliasYd.substring(0, 4));
-                alias.push(aliasYd);
-              }else{
-                btnArry.push('预定');
-                alias.push('预定');
-                that.setData({ aliasYd: '预定' });                
-              }
-            }else{
+  theme() {
+    var that = this;
+    post("/wxApi/c/theme", {}, function (ret) {
+      if (ret.code == 0) {
+        var btnArry = [];
+        var alias = [];
+        var num = ret.data.btnDc + ret.data.btnPd + ret.data.btnWm + ret.data.btnYd;
+        //首页四个按钮
+        var aliasPd = ret.data.aliasPd;
+        var aliasYd = ret.data.aliasYd;
+        var aliasWm = ret.data.aliasWm;
+        var aliasDc = ret.data.aliasDc;
+        var aliasQr = ret.data.aliasQr;
+        if (aliasQr) {
+          my.setStorageSync({ key: 'aliasQr', data: aliasQr });
+        } else {
+          my.removeStorageSync('aliasQr');
+        }
+        if (num == 3 && !ret.data.btnPd) {
+          if (ret.data.btnYd == 1) {
+            if (aliasYd != 1 && aliasYd) {
+              that.setData({ aliasYd: aliasYd.substring(0, 4) });
+              btnArry.push(aliasYd.substring(0, 4));
+              alias.push(aliasYd);
+            } else {
+              btnArry.push('预定');
               alias.push('预定');
-            }
-            if (ret.data.btnDc == 1) {
-              if (aliasDc != 1 && aliasDc) {
-                that.setData({ aliasDc: aliasDc.substring(0, 4) });
-                btnArry.push(aliasDc.substring(0, 4));
-                alias.push(aliasDc);
-              } else {
-                that.setData({ aliasDc: '点餐' });
-                btnArry.push('点餐');
-                alias.push('点餐');
-              }
-            }else{
-              alias.push('点餐');
-            }
-            if (ret.data.btnWm == 1) {
-              if (aliasWm != 1 && aliasWm) {
-                that.setData({ aliasWm: aliasWm.substring(0, 4) });
-                btnArry.push(aliasWm.substring(0, 4));
-                alias.push(aliasWm);
-              } else {
-                that.setData({ aliasWm: '外卖' });
-                btnArry.push('外卖');
-                alias.push('外卖'); 
-              }
-            }else{
-              alias.push('外卖');              
+              that.setData({ aliasYd: '预定' });
             }
           } else {
-            if (ret.data.btnYd == 1) {
-              if (aliasYd != 1 && aliasYd) {
-                that.setData({ aliasYd: aliasYd.substring(0, 4) });
-                btnArry.push(aliasYd.substring(0, 4));
-                alias.push(aliasYd);
-              } else {
-                that.setData({ aliasYd: '预定' });
-                btnArry.push('预定');
-                alias.push('预定');
-              }
-            }else{
+            alias.push('预定');
+          }
+          if (ret.data.btnDc == 1) {
+            if (aliasDc != 1 && aliasDc) {
+              that.setData({ aliasDc: aliasDc.substring(0, 4) });
+              btnArry.push(aliasDc.substring(0, 4));
+              alias.push(aliasDc);
+            } else {
+              that.setData({ aliasDc: '点餐' });
+              btnArry.push('点餐');
+              alias.push('点餐');
+            }
+          } else {
+            alias.push('点餐');
+          }
+          if (ret.data.btnWm == 1) {
+            if (aliasWm != 1 && aliasWm) {
+              that.setData({ aliasWm: aliasWm.substring(0, 4) });
+              btnArry.push(aliasWm.substring(0, 4));
+              alias.push(aliasWm);
+            } else {
+              that.setData({ aliasWm: '外卖' });
+              btnArry.push('外卖');
+              alias.push('外卖');
+            }
+          } else {
+            alias.push('外卖');
+          }
+        } else {
+          if (ret.data.btnYd == 1) {
+            if (aliasYd != 1 && aliasYd) {
+              that.setData({ aliasYd: aliasYd.substring(0, 4) });
+              btnArry.push(aliasYd.substring(0, 4));
+              alias.push(aliasYd);
+            } else {
+              that.setData({ aliasYd: '预定' });
+              btnArry.push('预定');
               alias.push('预定');
             }
-            if (ret.data.btnDc == 1) {
-              if (aliasDc != 1 && aliasDc) {
-                that.setData({ aliasDc: aliasDc.substring(0, 2) });
-                btnArry.push(aliasDc.substring(0, 2));
-                alias.push(aliasDc);
-              } else {
-                that.setData({ aliasDc: '点餐' });
-                btnArry.push('点餐');
-                alias.push('点餐'); 
-              }
-            }else{
-              alias.push('点餐');              
+          } else {
+            alias.push('预定');
+          }
+          if (ret.data.btnDc == 1) {
+            if (aliasDc != 1 && aliasDc) {
+              that.setData({ aliasDc: aliasDc.substring(0, 2) });
+              btnArry.push(aliasDc.substring(0, 2));
+              alias.push(aliasDc);
+            } else {
+              that.setData({ aliasDc: '点餐' });
+              btnArry.push('点餐');
+              alias.push('点餐');
             }
-            if (ret.data.btnWm == 1) {
-              if (aliasWm != 1 && aliasWm) {
-                that.setData({ aliasWm: aliasWm.substring(0, 4) });
-                btnArry.push(aliasWm.substring(0, 4));
-                alias.push(aliasWm);
-              } else {
-                that.setData({ aliasWm: '外卖' });
-                btnArry.push('外卖');
-                alias.push('外卖'); 
-              }
-            }else{
-              alias.push('外卖');              
+          } else {
+            alias.push('点餐');
+          }
+          if (ret.data.btnWm == 1) {
+            if (aliasWm != 1 && aliasWm) {
+              that.setData({ aliasWm: aliasWm.substring(0, 4) });
+              btnArry.push(aliasWm.substring(0, 4));
+              alias.push(aliasWm);
+            } else {
+              that.setData({ aliasWm: '外卖' });
+              btnArry.push('外卖');
+              alias.push('外卖');
             }
-            if (ret.data.btnPd == 1) {
-              if (aliasPd != 1 && aliasPd) {
-                that.setData({ aliasPd: aliasPd.substring(0, 4) });
-                btnArry.push(aliasPd.substring(0, 4));
-                alias.push(aliasPd);
-              } else {
-                that.setData({ aliasPd: '排队' });
-                btnArry.push('排队');
-                alias.push('排队');
-              }
-            }else{
+          } else {
+            alias.push('外卖');
+          }
+          if (ret.data.btnPd == 1) {
+            if (aliasPd != 1 && aliasPd) {
+              that.setData({ aliasPd: aliasPd.substring(0, 4) });
+              btnArry.push(aliasPd.substring(0, 4));
+              alias.push(aliasPd);
+            } else {
+              that.setData({ aliasPd: '排队' });
+              btnArry.push('排队');
               alias.push('排队');
-            }           
+            }
+          } else {
+            alias.push('排队');
           }
-          my.setStorageSync({key:'color',data:ret.data.color})
-          my.setStorage({
-            key: 'alias',
-            data: alias,
-          })
-          my.setStorage({
-            key: 'menuStyle',
-            data: ret.data.menuStyle
-          })
-          if (!my.getStorageSync({key:'color'})) {
-            my.setStorageSync({key:'color',data:'blue'});
-          }
-          // config.navBarColor(my.getStorageSync('color'));
-          that.setData({
-            btnNum: ret.data.btnDc + ret.data.btnPd + ret.data.btnWm + ret.data.btnYd,
-            btnArry: btnArry,
-            color: my.getStorageSync({key:'color'}).data,
-            btnHj: ret.data.btnHj
-          })
         }
-      },true)
-    },
+        my.setStorageSync({ key: 'color', data: ret.data.color })
+        my.setStorage({
+          key: 'alias',
+          data: alias,
+        })
+        my.setStorage({
+          key: 'menuStyle',
+          data: ret.data.menuStyle
+        })
+        if (!my.getStorageSync({ key: 'color' })) {
+          my.setStorageSync({ key: 'color', data: 'blue' });
+        }
+        // config.navBarColor(my.getStorageSync('color'));
+        that.setData({
+          btnNum: ret.data.btnDc + ret.data.btnPd + ret.data.btnWm + ret.data.btnYd,
+          btnArry: btnArry,
+          color: my.getStorageSync({ key: 'color' }).data,
+          btnHj: ret.data.btnHj
+        })
+      }
+    }, true)
+  },
+  load: function (self) {
+    // var that = self;
+    // that.setData({
+    //   loading: true
+    // })
+    // if(!my.getStorageSync('uid')){  //如果已经登录 设置监听授权信息 否则 不监听直接获取位置
+    //   app.onauthorsuccess=function(){
+    //     that.getdata('0', '0');
+    //     that.load(self);
+    //   }
+    //   return;
+    // }else{
+    //   app.onauthorsuccess=undefined;
+    // }
+    // app.getaddress({
+    //   successlocation:function(res){
+    //     that.setData({
+    //       lat: res.latitude,
+    //       lng: res.longitude
+    //     })
+    //     that.setData({
+    //       state: 1
+    //     })
+    //     that.getdata(that.data.lat, that.data.lng);
+    //   }
+    // })
+  },
+  up:function(){
+    this.setData({
+      stype:'up'
+    })
+  },
+  down:function(){
+    this.setData({
+      stype: 'down'
+    }) 
+  },
+  shutDown:function(){
+    console.log(11);
+    my.removeStorageSync({key:'sendInfo'});
+    this.setData({
+      sendShow:false
+    })
+  },
+  shutDown1:function(){
+    console.log(111);
+    this.setData({
+      robShow:false
+    })
+  },
+  goyhq:function(){
+    my.navigateTo({
+      url: '../mypage/qyhq',
+    })
+  },
   onReady() {
     // 页面加载完成
   },
   onShow() {
     // 页面显示
-    this.theme()
-  },
-  onHide() {
-    // 页面隐藏
-  },
-  onUnload() {
-    // 页面被关闭
-  },
-  onTitleClick() {
-    // 标题被点击
+    this.theme();
   },
   onPullDownRefresh() {
     // 页面被下拉
-  },
-  onReachBottom() {
-    // 页面被拉到底部
-  },
-  onShareAppMessage() {
-    // 返回自定义分享信息
+    this.getdata();
   },
 })
